b14069960a8ab60022af94b3583561945ccd9a44
Nguyễn Trí <tri789dr1998@gmail.com>
2019-10-14 10:05:42 +0700

    them class ThuongHieu

---
+++ ShopCuaTui/Models/ThuongHieu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCuaTui.Models
+{
+    public class ThuongHieu
+    {
+        [Key]
+        [MaxLength(50)]
+        public string MaTh { get; set; }
+        [Required(ErrorMessage = "*")]
+        [MaxLength(50)]
+        public string TenThuongHieu { get; set; }
+        [MaxLength(150)]
+        public string DiaChi { get; set; }
+        [MaxLength(50)]
+        public string DienThoai { get; set; }
+        [MaxLength(150)]
+        public string Logo { get; set; }
+    }
+}
