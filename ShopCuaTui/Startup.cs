c5a8ea916aff006b7185010b82edd95a9526eddb
dark3691011 <37678562+dark3691011@users.noreply.github.com>
2019-10-14 10:17:12 +0700

    ConnectionStrings to startup

--- ShopCuaTui/Startup.cs
+++ ShopCuaTui/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ShopCuaTui.Models;
 
 namespace ShopCuaTui
 {
@@ -33,6 +35,8 @@ namespace ShopCuaTui
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<MyDBContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("ShopCuaTui")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
